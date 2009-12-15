94a17aed8ca6301f0cf2ad4594782fd81a738326
trojanowski.ifuj <trojanowski.ifuj@35e1d7d4-e4ef-11de-b362-3513cf04e977>
2009-12-15 19:17:21 +0000

    thread-safe rng

--- trunk/montecarlo2/mc2/src/Random.cpp
+++ trunk/montecarlo2/mc2/src/Random.cpp
@@ -8,7 +8,11 @@
 #include "random01.h"
 
 double random01(){
-    return rangen::Instance()->Gen01();
+    double retval = 0.0;
+    // the rng is a singleton, so we need to make sure two threads don't access it simultaneously
+    #pragma omp critical
+    retval = rangen::Instance()->Gen01();
+    return retval;
 }
 int plusminusone(){
     if(random01()>0.5)
