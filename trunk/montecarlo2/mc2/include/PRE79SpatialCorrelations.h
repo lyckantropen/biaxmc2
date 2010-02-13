58b23d0c03a9a17a78eba0beb6349473f5411fac
trojanowski.ifuj <trojanowski.ifuj@35e1d7d4-e4ef-11de-b362-3513cf04e977>
2010-02-13 14:01:39 +0000

    tensors & correlations

--- trunk/montecarlo2/mc2/include/PRE79SpatialCorrelations.h
+++ trunk/montecarlo2/mc2/include/PRE79SpatialCorrelations.h
@@ -10,41 +10,120 @@
 
 #include "SpatialCorrelationEvolution.h"
 
-class Delta200Correlation:public SpatialCorrelationEvolution {
+/*
+ * Funkcje G 2,0,0 względem osi Z,X i Y
+ */
+
+class Delta200CorrelationZ:public SpatialCorrelationEvolution {
     using SpatialCorrelationEvolution::SerializerHelper;
     template<class serializer_t>
-    friend void operator|(serializer_t &, Delta200Correlation &);
+    friend void operator|(serializer_t &, Delta200CorrelationZ &);
     virtual double CalculateContraction(const Particle &a, const Particle &b){
         return 3.0/2.0*Rank2Contraction((a.GetQZ()-Identity(3)/3.0),(b.GetQZ()-Identity(3)/3.0));
     }
 public:
-    Delta200Correlation(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+    Delta200CorrelationZ(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
 };
 
-
-class Delta220Correlation:public SpatialCorrelationEvolution {
+class Delta200CorrelationX:public SpatialCorrelationEvolution {
     using SpatialCorrelationEvolution::SerializerHelper;
     template<class serializer_t>
-    friend void operator|(serializer_t &, Delta220Correlation &);
+    friend void operator|(serializer_t &, Delta200CorrelationX &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return 3.0/2.0*Rank2Contraction((a.GetQX()-Identity(3)/3.0),(b.GetQZ()-Identity(3)/3.0));
+    }
+public:
+    Delta200CorrelationX(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
+
+class Delta200CorrelationY:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta200CorrelationY &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return 3.0/2.0*Rank2Contraction((a.GetQY()-Identity(3)/3.0),(b.GetQZ()-Identity(3)/3.0));
+    }
+public:
+    Delta200CorrelationY(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
+
+/*
+ * Funkcje G 2,2,0 względem osi Z,X,Y
+ */
+
+class Delta220CorrelationZ:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta220CorrelationZ &);
     virtual double CalculateContraction(const Particle &a, const Particle &b){
         return std::sqrt(3.0)/2.0*Rank2Contraction((a.GetQX()-a.GetQY()),(b.GetQZ()-Identity(3)/3.0));
     }
 public:
-    Delta220Correlation(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+    Delta220CorrelationZ(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
 };
 
-class Delta222Correlation:public SpatialCorrelationEvolution {
+class Delta220CorrelationX:public SpatialCorrelationEvolution {
     using SpatialCorrelationEvolution::SerializerHelper;
     template<class serializer_t>
-    friend void operator|(serializer_t &, Delta222Correlation &);
+    friend void operator|(serializer_t &, Delta220CorrelationX &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return std::sqrt(3.0)/2.0*Rank2Contraction((a.GetQY()-a.GetQZ()),(b.GetQX()-Identity(3)/3.0));
+    }
+public:
+    Delta220CorrelationX(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
+
+class Delta220CorrelationY:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta220CorrelationY &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return std::sqrt(3.0)/2.0*Rank2Contraction((a.GetQZ()-a.GetQX()),(b.GetQY()-Identity(3)/3.0));
+    }
+public:
+    Delta220CorrelationY(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
+
+/*
+ * Funkcje G 2,2,2 względem osi Z,X,Y
+ */
+
+class Delta222CorrelationZ:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta222CorrelationZ &);
     virtual double CalculateContraction(const Particle &a, const Particle &b){
         return 0.5*Rank2Contraction((a.GetQX()-a.GetQY()),(b.GetQX()-b.GetQY()));
     }
 public:
-    Delta222Correlation(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+    Delta222CorrelationZ(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
 };
 
+class Delta222CorrelationX:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta222CorrelationX &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return 0.5*Rank2Contraction((a.GetQY()-a.GetQZ()),(b.GetQY()-b.GetQZ()));
+    }
+public:
+    Delta222CorrelationX(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
 
+class Delta222CorrelationY:public SpatialCorrelationEvolution {
+    using SpatialCorrelationEvolution::SerializerHelper;
+    template<class serializer_t>
+    friend void operator|(serializer_t &, Delta222CorrelationY &);
+    virtual double CalculateContraction(const Particle &a, const Particle &b){
+        return 0.5*Rank2Contraction((a.GetQZ()-a.GetQX()),(b.GetQZ()-b.GetQX()));
+    }
+public:
+    Delta222CorrelationY(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
+};
+
+/**
+ * Funkcja korelacji G 3,2,2
+ */
 class Delta322Correlation:public SpatialCorrelationEvolution {
     using SpatialCorrelationEvolution::SerializerHelper;
     template<class serializer_t>
@@ -68,18 +147,56 @@ public:
     ParityCorrelation(Lattice * l=NULL, int nc=0):SpatialCorrelationEvolution(l,nc){}
 };
 
+/*
+ * Serializery dla funkcji G 2,0,0
+ */
 template<class serializer_t>
-void operator|(serializer_t & s, Delta200Correlation & e){
+void operator|(serializer_t & s, Delta200CorrelationZ & e){
     e.SerializerHelper<serializer_t>(s);
 }
 template<class serializer_t>
-void operator|(serializer_t & s, Delta220Correlation & e){
+void operator|(serializer_t & s, Delta200CorrelationX & e){
     e.SerializerHelper<serializer_t>(s);
 }
 template<class serializer_t>
-void operator|(serializer_t & s, Delta222Correlation & e){
+void operator|(serializer_t & s, Delta200CorrelationY & e){
     e.SerializerHelper<serializer_t>(s);
 }
+/*
+ * Serializery dla funkcji G 2,2,0
+ */
+template<class serializer_t>
+void operator|(serializer_t & s, Delta220CorrelationZ & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+template<class serializer_t>
+void operator|(serializer_t & s, Delta220CorrelationX & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+template<class serializer_t>
+void operator|(serializer_t & s, Delta220CorrelationY & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+
+/*
+ * Serializer dla funkcji G 2,2,2
+ */
+template<class serializer_t>
+void operator|(serializer_t & s, Delta222CorrelationZ & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+template<class serializer_t>
+void operator|(serializer_t & s, Delta222CorrelationX & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+template<class serializer_t>
+void operator|(serializer_t & s, Delta222CorrelationY & e){
+    e.SerializerHelper<serializer_t>(s);
+}
+
+
+
+
 template<class serializer_t>
 void operator|(serializer_t & s, Delta322Correlation & e){
     e.SerializerHelper<serializer_t>(s);
