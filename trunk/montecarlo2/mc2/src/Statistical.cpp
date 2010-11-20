6171347436e10a4394acbd6cf44a9567ad2f7540
trojanowski.ifuj <trojanowski.ifuj@35e1d7d4-e4ef-11de-b362-3513cf04e977>
2010-11-20 22:24:23 +0000

    recalculate

--- trunk/montecarlo2/mc2/src/Statistical.cpp
+++ trunk/montecarlo2/mc2/src/Statistical.cpp
@@ -1,18 +1,22 @@
 #include "Statistical.h"
 
 Value CalculateFluctuation(const vect & variable,const int & acc_idx){
-        vect fluct3(acc_idx+1);
-        for(int i=0;i<(acc_idx+1);i++){
-            vect e(acc_idx+1);
-            vect e2(acc_idx+1);
-            for(int j=0;j<(acc_idx+1);j++){
-                int t = (acc_idx)*random01();
-                e[j]=variable[t];
-                e2[j]=variable[t]*variable[t];
-            }
-            fluct3[i]=(double(Mean(e2))-double(Mean(e))*double(Mean(e)));
+    int size=acc_idx+1;
+    if(acc_idx==0)
+        size=variable.size();
+
+    vect fluct3(size);
+    for(int i=0;i<(size);i++){
+        vect e(size);
+        vect e2(size);
+        for(int j=0;j<(size);j++){
+            int t = (size)*random01();
+            e[j]=variable[t];
+            e2[j]=variable[t]*variable[t];
         }
-        return Mean(fluct3);
+        fluct3[i]=(double(Mean(e2))-double(Mean(e))*double(Mean(e)));
+    }
+    return Mean(fluct3);
 }
 
 std::ostream & operator<<(std::ostream & s, Value & v){
