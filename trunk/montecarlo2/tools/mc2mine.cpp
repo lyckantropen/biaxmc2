05a70e01d6391ec32ace0de74460b03f1d916605
trojanowski.ifuj <trojanowski.ifuj@35e1d7d4-e4ef-11de-b362-3513cf04e977>
2011-07-06 10:38:50 +0000

    recalculate

--- trunk/montecarlo2/tools/mc2mine.cpp
+++ trunk/montecarlo2/tools/mc2mine.cpp
@@ -132,6 +132,8 @@ void    table_output(const std::string & data_type,const std::vector<std::string
                         double tau = std::atof(h_data[i][index(h_columns,"tau")].c_str());
                         double h = std::atof(h_data[i][index(h_columns,"field")].c_str());
                         
+                        std::cout << "t: " << t << ", l: " << ", tau: " << tau << ", h:" << h << std::endl;
+                        
 			//PRE79StandardHamiltonian H(wwg[i].Temperature(),wwg[i].Lambda(),wwg[i].Tau(),wwg[i].Field());
 			
                         PRE79StandardHamiltonian H(t,l,tau,h);
